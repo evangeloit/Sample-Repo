02a9223c9337081cc807b0c7f8b22f682a0929ef
Evangelos Germenis <evangeloit@gmail.com>
2020-11-01 03:19:32 +0100

    Removed some code

--- script.py
+++ script.py
@@ -1,19 +1,7 @@
-import math
-import sys
 from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet('world'))
-# print(greet('Evan'))
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
